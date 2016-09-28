702466d600ad6beab1cd67b5e100718b4587ce9c
scutlxj <scutlxj@163.com>
2016-09-28 10:59:14 +0800

    9.28

---
+++ IFFConversionLibrary/src/main/scala/com/boc/iff/exception/BaseException.scala
@@ -0,0 +1,16 @@
+package com.boc.iff.exception
+
+/**
+  * Created by birdie on 9/18/16.
+  */
+sealed trait BaseException {
+}
+
+case class RecordNumberErrorException(val message:String) extends Exception(message)
+
+case class MaxErrorNumberException(val message:String) extends Exception(message)
+
+case class RecordNotFixedException(val message:String) extends Exception(message)
+
+case class MaxBlankNumberException(val message:String) extends Exception(message)
+
