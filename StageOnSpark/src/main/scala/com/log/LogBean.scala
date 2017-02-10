11449b0c5e93bf8f757f5ee93353b4c608bcc763
visionskyGH <8111vision8111>
2017-02-10 14:52:00 +0800

    tzm

---
+++ StageOnSpark/src/main/scala/com/log/LogBean.scala
@@ -0,0 +1,14 @@
+package com.log
+
+/**
+  * Created by tangzm on 2017/2/9.
+  */
+class LogBean {
+  var hostName:String=""
+  var jobID:String=""
+  var threadID:String=""
+  var msg:String=""
+  def toLogMsg()={
+    "HOSTNAME="+hostName+"|JOBID="+jobID+"|THREADID="+threadID+"|MESSAGE="+msg+";"
+  }
+}
